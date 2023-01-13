d39c8106cc87c3ff73d4e2d15502af8a02d67fd3
Lece <lece619@gmail.com>
2023-01-13 22:59:53 +0900

    Feat(AboutCommand): try about toolbox

--- com.study.rcptutorial01/src/org/o7planning/tutorial/rcp/cmd/AboutCommand.java
+++ com.study.rcptutorial01/src/org/o7planning/tutorial/rcp/cmd/AboutCommand.java
@@ -5,59 +5,29 @@ import java.util.Map;
 import com.study.rcputorial01.dialog.AboutDialog;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.commands.AbstractHandler;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-public class AboutCommand extends AbstractHandler implements IHandler {
-
-	@Override
-	public void addHandlerListener(IHandlerListener handlerListener) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-
-	}
+public class AboutCommand extends AbstractHandler{
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		// TODO Auto-generated method stub
 		Shell shell = HandlerUtil.getActiveWorkbenchWindow(event).getShell();
-		AboutDialog dialog = new AboutDialog(shell, SWT.DIALOG_TRIM
-				| SWT.APPLICATION_MODAL);
+		AboutDialog dialog = new AboutDialog(shell, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
 		dialog.open();
 		return null;
 	}
 
 	@Override
-	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isHandled() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void removeHandlerListener(IHandlerListener handlerListener) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
 	public Object execute(Map parameterValuesByName) throws org.eclipse.ui.commands.ExecutionException {
-
+		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
 
 }
