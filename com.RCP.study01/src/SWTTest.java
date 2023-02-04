3632e0448af3d14f93d8647c653a9e82f626728f
Lece <lece619@gmail.com>
2023-02-05 03:01:27 +0900

    feat: Layout 설정에 대해서 학습필요

--- com.RCP.study01/src/SWTTest.java
+++ com.RCP.study01/src/SWTTest.java
@@ -1,4 +1,6 @@
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
@@ -7,8 +9,11 @@ public class SWTTest {
     public static void main(String[] args) {
         Display display = new Display();
         Shell shell = new Shell(display);
+        shell.setLayout(new FillLayout());
+        
+        Composite composite = new Composite(shell, SWT.NORMAL);
 
-        Label label = new Label(shell, SWT.NORMAL);
+        Label label = new Label(composite, SWT.NORMAL);
         label.setText("hi");
 
         shell.open();
